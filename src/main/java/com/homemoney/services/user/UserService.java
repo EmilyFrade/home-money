0d3a7e77b63c8f3631c11e2e0bbbc242ab1794da
anaflaviapizati <anaflaviapizati@hotmail.com>
2024-12-31 02:15:27 -0300

    feat: novo layout teste e criação de usuarios

---
+++ src/main/java/com/homemoney/services/user/UserService.java
@@ -0,0 +1,31 @@
+package com.homemoney.services.user;
+
+import com.homemoney.model.user.User;
+import com.homemoney.repositories.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}

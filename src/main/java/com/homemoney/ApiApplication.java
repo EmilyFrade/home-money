57f5764378a24b52b4b423def9f041ae63ee30d0
Emily Frade <emilyfrade12@gmail.com>
2025-01-09 18:48:12 -0300

    feat: vencimento automático de despesa

--- src/main/java/com/homemoney/ApiApplication.java
+++ src/main/java/com/homemoney/ApiApplication.java
@@ -2,10 +2,12 @@ package com.homemoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.homemoney.config.DotenvInitializer;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/homemoney/repositories/expense/ExpenseRepository.java
+++ src/main/java/com/homemoney/repositories/expense/ExpenseRepository.java
@@ -1,8 +1,11 @@
 package com.homemoney.repositories.expense;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.homemoney.model.expense.Expense;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByStatus(Expense.Status status);
 }

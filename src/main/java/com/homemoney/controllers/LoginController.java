f4d4621271b99eb7a510c8acfb429b0368c52f7f
anaflaviapizati <anaflaviapizati@hotmail.com>
2025-03-19 17:31:45 -0300

    validação login

--- src/main/java/com/homemoney/controllers/LoginController.java
+++ src/main/java/com/homemoney/controllers/LoginController.java
@@ -1,13 +1,18 @@
 package com.homemoney.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute("errorMessage", "E-mail ou senha inválidos!");
+        }
         return "login";
     }
 }
